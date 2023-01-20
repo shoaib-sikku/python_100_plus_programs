ed9aae0714434a06190d3dec1c49cf1fd0d29eb0
sikku <ms1654922@gmail.com>
2023-01-20 19:12:43 +0530

    20/01/2023

---
+++ 05_swap_two_num.py
@@ -0,0 +1,14 @@
+print('****Swap Two Numbers Program****')
+
+a=float(input('Enter your first number: \n'))
+b=float(input('Enter your second number: \n'))
+
+# First method
+# c=a
+# a=b
+# b=c
+
+# Second method.
+a,b = b,a
+
+print(f'value of first number is {a} value of second number is {b}')
